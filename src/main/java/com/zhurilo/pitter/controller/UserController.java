4b26b6a20d45a54ad29b133d9d8d79204c22863d
Jack <evzhurilo@gmail.com>
2023-03-19 21:24:40 +0300

    Old files were deleted

--- src/main/java/com/zhurilo/pitter/controller/UserController.java
+++ src/main/java/com/zhurilo/pitter/controller/UserController.java
@@ -5,8 +5,6 @@ import com.zhurilo.pitter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,14 +16,8 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/home/signup")
-    public String showAddUserForm(Model model) {
-        model.addAttribute("title","Registration");
-        return "sign-up";
-    }
-
     @PostMapping("/home/signup")
-    public String signupSubmit(@RequestParam String name, @RequestParam String surname, @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") Date dateOfBirth,
+    public String signupSubmit(@RequestParam String name, @RequestParam String surname, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateOfBirth,
                                @RequestParam String email, @RequestParam String password) {
         User user = new User();
         user.setName(name);
@@ -36,6 +28,5 @@ public class UserController {
         userRepository.save(user);
         return "redirect:/home";
     }
-
 
 }
