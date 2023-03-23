888380f75492513d8e92df06e92cc262d6fa2b0a
Jack <evzhurilo@gmail.com>
2023-03-24 01:14:01 +0300

    Relocated files to directly to the packages with changed names.

--- src/test/java/com/zhurilo/pitter/controller/UserControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.zhurilo.pitter.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserControllerTest {
-
-}
--- src/test/java/com/zhurilo/pitter/controllers/UserControllerTest.java
+++
@@ -1,5 +0,0 @@
-package com.zhurilo.pitter.controllers;
-
-class UserControllerTest {
-
-}
