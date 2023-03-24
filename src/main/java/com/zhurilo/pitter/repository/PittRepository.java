223d887894bfa8d57817cc5fa2d5f6d2b727326d
Jack <evzhurilo@gmail.com>
2023-03-24 23:55:40 +0300

    added method findPittsByUserId, this method returns list of all user's pitts.

--- src/main/java/com/zhurilo/pitter/repository/PittRepository.java
+++ src/main/java/com/zhurilo/pitter/repository/PittRepository.java
@@ -3,5 +3,9 @@ package com.zhurilo.pitter.repository;
 import com.zhurilo.pitter.entity.Pitt;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface PittRepository extends CrudRepository<Pitt,Long> {
+    List<Pitt> findPittsByUserId(Long user_id);
 }
